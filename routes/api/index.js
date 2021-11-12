5648d1f38d0ba8cb1aa9c34aaeedd0ef8643175c
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2021-11-11 20:30:02 -0600

    add code to routes/api/index.js

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
