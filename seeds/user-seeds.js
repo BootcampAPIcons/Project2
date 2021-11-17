93bfcd2c0f401556ede80fc04e38f8784d6f54a1
Lance Kelley <lancekkcodes@gmail.com>
2021-11-16 20:11:22 -0600

    user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,9 +1,29 @@
 const {User} = require('../models');
 
 const userData = [
-  {},
+  {
+    username: "quinn",
+    email: "quinn@example.com",
+    password: "q039u4th*(7ho",
+    points: 99,
+    character_name: "qontestant",
+  },
+  {
+    username: "jane_doe",
+    email: "jad@example.com",
+    password: "P@ssw0rD",
+    points: 83,
+    character_name: "janette",
+  },
+  {
+    username: "john_doe",
+    email: "jod@example.com",
+    password: "p@ssw0rd",
+    points: 55,
+    character_name: "johnny",
+  },
 ];
 
-const seedUsers = require('../models');
+const seedusers = require('../models');
 
-module.exports = seedUsers;
+module.exports = seedusers;
