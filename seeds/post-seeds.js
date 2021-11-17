f0a1ea74c64012fd1eed034e2ec8aba7e1302fca
Lance Kelley <lancekkcodes@gmail.com>
2021-11-16 20:36:07 -0600

    post seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,7 +1,26 @@
 const {Post} = require('../models');
 
 const postData = [
-  {}
+  {
+    title: "My first post!",
+    post_url: "posts/1",
+    user_id: 1
+  },
+  {
+    title: "MY first post",
+    post_url: "posts/2",
+    user_id: 2
+  },
+  {
+    title: "My second post",
+    post_url: "posts/3",
+    user_id: 2
+  },
+  {
+    title: "late to the party! (first post)",
+    post_url: "posts/4",
+    user_id: 3
+  },
 ];
 
 const seedPosts = () => Post.bulkCreate(postData);
