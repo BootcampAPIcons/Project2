4b450a8da5db81b4e8efe44b110f1d93956b37f4
Lance Kelley <lancekkcodes@gmail.com>
2021-11-18 10:30:23 -0600

    incremental progress

--- public/js/encounter.js
+++ public/js/encounter.js
@@ -1,3 +1,7 @@
+const encounterWindow = document.querySelector('.encounter-window');
+const enemyDesc = document.querySelector('.enemy-description');
+const eventDisplay = document.querySelector('.event-display');
+const controls = document.querySelector('.encounter-controls');
 
 class Enemy {
   constructor(type, hp, atk, reward) {
@@ -25,5 +29,31 @@ class Ogre extends Enemy {
     super('Ogre', 50, 10, 75);
   }
 };
+
+const enemyClasses = [ Goblin, Orc, Ogre ];
+
+const selectEnemy = () => enemyClasses[Math.floor(Math.random() * enemyClasses.length)];
+
+const startEncounter = () => {
+  let enemy = new (selectEnemy())();
+  console.log(enemy.type);
+  enemy.hp = enemy.maxHP;
+  let player = {hp: 50, atk: 10};
+}
+
+const victoryHandler = async (enemy) => {
+  let result = await fetch(`${window.location.host}/api/encounter/win`, {
+    method: 'POST',
+    mode: "same-origin",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(enemy)
+  });
+  console.log(result);
+  eventDisplay.textContent = result;
+}
+
+startEncounter();
 
 // module.exports = {Enemy, Goblin, Orc, Ogre};
