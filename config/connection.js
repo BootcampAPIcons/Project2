5a74ff479a5a244899af254b02a07a329674c1ab
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2021-11-18 16:16:16 -0600

    add single-post and login handlebars

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connection to database, pass in username and password
+// create connection to database, pass in username and password
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 //     host: 'localhost',
 //     dialect: 'mysql',
