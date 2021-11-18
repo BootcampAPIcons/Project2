7f8f6dccdf5237390f5f18220b00fbaeca035035
Lance Kelley <lancekkcodes@gmail.com>
2021-11-18 17:47:12 -0600

    very raw styles, single-post skeleton

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -78,6 +78,7 @@ router.get('/post/:id', (req, res) => {
 
       const post = dbPostData.get({ plain: true });
 
+      console.log(post);
       res.render('single-post', {
         post,
         loggedIn: req.isAuthenticated()
